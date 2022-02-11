5e8f86a63480bfedf6f54195f6323ef77fc6c5a8
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-02-10 20:12:47 -0600

    Completed documentation.

--- Warmup-1/In3050.java
+++ Warmup-1/In3050.java
@@ -1,5 +1,21 @@
+/**
+* This class includes the declaration, implementation, and testing for
+* the in3050 method.
+*
+* @author NicholasRaynes
+* @version 0.0.1
+* @see https://codingbat.com/prob/p132134
+*/
 public class In3050
 {
+  /**
+  * Given 2 int values, return true if they are both in the range 30..40 
+  * inclusive, or they are both in the range 40..50 inclusive.
+  *
+  * @param a The first integer.
+  * @param b The second integer.
+  * @return Whether or not the two numbers are within their respective ranges.
+  */
   public boolean in3050(int a, int b) 
   {
     if (a >= 30 && a <= 40 && b >= 30 && b <= 40) 
