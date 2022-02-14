c8475c17a99fa43bc56a8abff8f29d781f01c117
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-02-14 14:58:13 -0600

    Completed documentation.

--- Warmup-2/ArrayCount9.java
+++ Warmup-2/ArrayCount9.java
@@ -1,3 +1,12 @@
+/**
+* This class includes the declaration, implementation, and testing for
+* the arrayCount9 method.
+*
+* @author NicholasRaynes
+* @version 0.0.1
+* @see https://codingbat.com/prob/p184031
+*/
+
 public class ArrayCount9
 {
   public static void main(String[] args)
@@ -11,6 +20,13 @@ public class ArrayCount9
     System.out.printf("Test #2 - arrayCount9([1, 9, 9])\nOutput:%d\n", arrayCount9(test2));
     System.out.printf("Test #3 - arrayCount9([1, 9, 9, 3, 9])\nOutput:%d\n", arrayCount9(test3));
   }
+  
+  /**
+  * Given an array of ints, return the number of 9's in the array.
+  *
+  * @param nums The array to be searched through.
+  * @return count How many times the integer 9 is present within the array provided.
+  */
   public static int arrayCount9(int[] nums)
   {
     int count = 0;
