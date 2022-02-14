eac0379d0c122f43277b2d416a8e42feaad22bb7
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-02-14 14:55:55 -0600

    Added some testing outputs.

--- Warmup-2/ArrayCount9.java
+++ Warmup-2/ArrayCount9.java
@@ -1,17 +1,28 @@
 public class ArrayCount9
 {
+  public static void main(String[] args)
+  {
+    // Testing
+    int[] test1 = {1, 2, 9};
+    int[] test2 = {1, 9, 9};
+    int[] test3 = {1, 9, 9, 3, 9};
+    
+    System.out.printf("Test #1 - arrayCount9([1, 2, 9])\nOutput:%d\n", arrayCount9(test1));
+    System.out.printf("Test #2 - arrayCount9([1, 9, 9])\nOutput:%d\n", arrayCount9(test2));
+    System.out.printf("Test #3 - arrayCount9([1, 9, 9, 3, 9])\nOutput:%d\n", arrayCount9(test3));
+  }
   public static int arrayCount9(int[] nums)
   {
-      int count = 0;
+    int count = 0;
   
-      for(int i = 0; i < nums.length; i++)
+    for(int i = 0; i < nums.length; i++)
+    {
+      if(nums[i] == 9)
       {
-        if(nums[i] == 9)
-        {
-          count++;
-        }
+        count++;
       }
+    }
   
-      return count;
+    return count;
   }
 }
