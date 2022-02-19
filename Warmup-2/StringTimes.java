ca1ed0d36e269ac6f083a77c44ab05260636e73d
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-02-19 15:50:21 -0600

    Added some output tests for the method.

--- Warmup-2/StringTimes.java
+++ Warmup-2/StringTimes.java
@@ -1,5 +1,13 @@
 public class StringTimes
 {
+  public static void main(String[] args)
+  {
+      // Testing
+      System.out.printf("Test #1 - stringTimes('Hi', 2)\nOutput:%s\n", stringTimes("Hi", 2));
+      System.out.printf("Test #2 - stringTimes('Hi', 3)\nOutput:%s\n", stringTimes("Hi", 3));
+      System.out.printf("Test #3 - stringTimes('Hi', 1)\nOutput:%s\n", stringTimes("Hi", 1));
+  }
+  
   public static String stringTimes(String input, int factor) 
   {
     String output = "";
