df7de8ffcc9ce0a5c7861f4c5144b3b779393a78
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-02-22 13:53:41 -0600

    Added some testing for the output.

--- Warmup-2/CountXX.java
+++ Warmup-2/CountXX.java
@@ -1,5 +1,13 @@
 public class CountXX
 {
+  public static void main(String[] args)
+  {
+    // Testing
+    System.out.printf("Test #1 - countXX('abcxx')\nOutput:%s\n", countXX("abcxx"));
+    System.out.printf("Test #2 - countXX('xxx')\nOutput:%s\n", countXX("xxx"));
+    System.out.printf("Test #3 - countXX('xxxx')\nOutput:%s\n", countXX("xxxx"));
+  }
+  
   public static int countXX(String input) 
   {
     int count = 0;
