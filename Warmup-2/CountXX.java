b612dec42240ede3afb6d247cc67e596d3f5db50
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-02-22 13:57:06 -0600

    Completed documentation.

--- Warmup-2/CountXX.java
+++ Warmup-2/CountXX.java
@@ -1,3 +1,11 @@
+/**
+* This class includes the declaration, implementation, and testing for
+* the countXX method.
+*
+* @author NicholasRaynes
+* @version 0.0.1
+* @see https://codingbat.com/prob/p194667
+*/
 public class CountXX
 {
   public static void main(String[] args)
@@ -8,6 +16,13 @@ public class CountXX
     System.out.printf("Test #3 - countXX('xxxx')\nOutput:%s\n", countXX("xxxx"));
   }
   
+  /**
+  * This method counts the number of "xx" in the given string. We'll say that 
+  * overlapping is allowed, so "xxx" contains 2 "xx".
+  *
+  * @param input The string to be scanned through.
+  * @return count The amount of times "xx" appears within the given string.
+  */
   public static int countXX(String input) 
   {
     int count = 0;
