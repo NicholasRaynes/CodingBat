c5c3dd4d7364f9919b0bde7d06c50aa07dca147c
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-02-27 15:28:01 -0600

    Completed some output testing.

--- Warmup-1/HasTeen.java
+++ Warmup-1/HasTeen.java
@@ -1,5 +1,18 @@
 public class HasTeen
 {
+    public static void main(String[] args)
+    {
+        // Testing
+        System.out.println("Test #1 - hasTeen(13, 20, 10)");
+    	System.out.printf("Output: %b\n", hasTeen(13, 20, 10));
+    	
+    	System.out.println("Test #2 - hasTeen(20, 19, 10)");
+    	System.out.printf("Output: %b\n", hasTeen(20, 19, 10));
+    	
+    	System.out.println("Test #3 - hasTeen(20, 10, 13)");
+    	System.out.printf("Output: %b\n", hasTeen(20, 10, 13));
+    }
+    
     public static boolean hasTeen(int a, int b, int c) 
     {
         return (a>=13 && a<=19) || (b>=13 && b<=19) || (c>=13 && c<=19);
