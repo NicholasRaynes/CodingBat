9ce7777f2ac75ae0a539438390d416f8b28f8391
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-02-28 13:37:05 -0600

    Completed documentation for class.

--- Warmup-1/HasTeen.java
+++ Warmup-1/HasTeen.java
@@ -1,3 +1,11 @@
+/**
+* This class includes the declaration, implementation, and testing for
+* the hasTeen method.
+*
+* @author NicholasRaynes
+* @version 0.0.1
+* @see https://codingbat.com/prob/p178986
+*/
 public class HasTeen
 {
     public static void main(String[] args)
@@ -12,7 +20,7 @@ public class HasTeen
     	System.out.println("Test #3 - hasTeen(20, 10, 13)");
     	System.out.printf("Output: %b\n", hasTeen(20, 10, 13));
     }
-    
+   
     public static boolean hasTeen(int a, int b, int c) 
     {
         return (a >= 13 && a <= 19) || (b >= 13 && b <= 19) || (c >= 13 && c <= 19);
