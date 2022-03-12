6cc6c56752e60c6a7cfa6daa2c3b5c57a15afa5f
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-03-12 16:29:24 -0600

    Completed some tests for the method.

--- Warmup-1/StartHi.java
+++ Warmup-1/StartHi.java
@@ -1,5 +1,18 @@
 public class StartHi
 {
+  public static void main(String[] args)
+  {
+    // Testing
+    System.out.println("Test #1 - startHi('hi there')");
+    System.out.printf("Output: %b\n", startHi("hi there"));
+    	
+    System.out.println("Test #2 - startHi('hi')");
+    System.out.printf("Output: %b\n", startHi("hi"));
+    	
+    System.out.println("Test #3 - startHi('hello hi')");
+    System.out.printf("Output: %b\n", startHi("hello hi"));
+  }
+  
   public static boolean startHi(String input) 
   {
     if (input.length() < 2) 
