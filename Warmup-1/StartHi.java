27dab0bbc02a51ba705cbf72441afbcebd430c71
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-03-13 21:15:02 -0500

    Completed documentation for the class.

--- Warmup-1/StartHi.java
+++ Warmup-1/StartHi.java
@@ -1,3 +1,11 @@
+/**
+* This class includes the declaration, implementation, and testing for
+* the startHi method.
+*
+* @author NicholasRaynes
+* @version 0.0.1
+* @see https://codingbat.com/prob/p191022
+*/
 public class StartHi
 {
   public static void main(String[] args)
