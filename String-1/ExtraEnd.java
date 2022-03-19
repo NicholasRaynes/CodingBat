727d16295ebfe5be518174622194bfd43df4e6bd
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-03-19 18:50:53 -0500

    Create ExtraEnd.java

---
+++ String-1/ExtraEnd.java
@@ -0,0 +1,14 @@
+public class ExtraEnd
+{
+  public static String extraEnd(String str) 
+  {
+    String lastTwo = "";
+  
+    if(str.length() >= 2)
+    {
+      lastTwo = str.substring(str.length() - 2);
+    }
+  
+    return lastTwo + lastTwo + lastTwo;
+  }
+}
