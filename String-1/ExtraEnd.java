a96e0e58a6e44f465a6cd2bc3bbeb18167955b35
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-03-19 19:01:06 -0500

    Completed some output tests for the method.

--- String-1/ExtraEnd.java
+++ String-1/ExtraEnd.java
@@ -1,5 +1,18 @@
 public class ExtraEnd
 {
+  public static void main(String[] args)
+  {
+    // Testing
+    System.out.println("Test #1 - extraEnd('Hello')");
+    System.out.printf("Output: %s\n", extraEnd("Hello"));
+    
+    System.out.println("Test #2 - extraEnd('ab')");
+    System.out.printf("Output: %s\n", extraEnd("ab"));
+    
+    System.out.println("Test #3 - extraEnd('Test')");
+    System.out.printf("Output: %s\n", extraEnd("Test"));
+  }
+  
   public static String extraEnd(String str) 
   {
     String lastTwo = "";
