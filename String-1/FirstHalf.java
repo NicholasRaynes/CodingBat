74260d889bfe4f08c4290d099dac5a340e7889b7
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-03-25 16:01:37 -0500

    Completed some testing for the method.

--- String-1/FirstHalf.java
+++ String-1/FirstHalf.java
@@ -1,5 +1,18 @@
 public class FirstHalf
 {
+    public static void main(String[] args)
+    {
+        // Testing
+        System.out.println("Test #1 - firstHalf('WooHoo')");
+        System.out.printf("Output: %s\n", firstHalf("WooHoo"));
+        
+        System.out.println("Test #2 - firstHalf('HelloThere')");
+        System.out.printf("Output: %s\n", firstHalf("HelloThere"));
+        
+        System.out.println("Test #3 - firstHalf('abcdef')");
+        System.out.printf("Output: %s\n", firstHalf("abcdef"));
+    }
+    
     public static String firstHalf(String str) 
     {
         String half = "";
