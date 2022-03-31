721d4af164e3c3b69a523fdd74b0659c115a5350
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-03-31 12:31:55 -0500

    Completed some tests for the method.

--- Warmup-2/Array667.java
+++ Warmup-2/Array667.java
@@ -1,5 +1,17 @@
 public class Array667
 {
+     public static void main(String[] args)
+    {
+        int[] test1 = {6, 6, 2};
+        int[] test2 = {6, 6, 2, 6};
+        int[] test3 = {6, 7, 2, 6};
+        
+        // Testing
+        System.out.printf("Test #1 - array667[6, 6, 2]\nOutput:%d\n", array667(test1));
+        System.out.printf("Test #2 - array667[6, 6, 2, 6]\nOutput:%d\n", array667(test2));
+        System.out.printf("Test #3 - array667[6, 7, 2, 6]\nOutput:%d\n", array667(test3));
+    }
+    
     public static int array667(int[] nums) 
     {
         int count = 0;
