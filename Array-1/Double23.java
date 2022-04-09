3db014fed4da0cb9da9a0c153da8acf0b6c5b6a4
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-04-08 21:52:25 -0500

    Completed documentation for the class.

--- Array-1/Double23.java
+++ Array-1/Double23.java
@@ -1,3 +1,11 @@
+/**
+* This class includes the declaration, implementation, and testing for
+* the double23 method.
+*
+* @author NicholasRaynes
+* @version 0.0.1
+* @see https://codingbat.com/prob/p145365
+*/
 public class Double23
 {
    public static void main(String[] args)
