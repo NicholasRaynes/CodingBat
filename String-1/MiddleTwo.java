659436b22c8e9c02d454f40668d113c4c6fc16c2
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-04-14 22:26:53 -0500

    Completed some testing for the method.

--- String-1/MiddleTwo.java
+++ String-1/MiddleTwo.java
@@ -1,5 +1,18 @@
 public class HelloWorld 
 {
+    public static void main(String[] args)
+    {
+        // Testing
+        System.out.println("Test #1 - middleTwo('string')");
+        System.out.printf("Output: %s\n", middleTwo("string"));
+        
+        System.out.println("Test #2 - middleTwo('code')");
+        System.out.printf("Output: %s\n", middleTwo("code"));
+        
+        System.out.println("Test #3 - middleTwo('Practice')");
+        System.out.printf("Output: %s\n", middleTwo("Practice"));
+    }
+    
     public static String middleTwo(String str) 
     {
         int half = str.length() / 2;
