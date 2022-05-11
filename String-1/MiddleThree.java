264a4ead9959f2fb1ffd5eff4f024569da045d21
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-05-11 18:48:55 -0500

    Completed some method tests.

--- String-1/MiddleThree.java
+++ String-1/MiddleThree.java
@@ -1,5 +1,18 @@
 public class MiddleThree
 {
+    public static void main(String[] args)
+    {
+        // Testing
+        System.out.println("Test #1 - middleThree('Candy')");
+        System.out.printf("Output: %s\n", middleThree("Candy"));
+        
+        System.out.println("Test #2 - middleThree('and')");
+        System.out.printf("Output: %s\n", middleThree("and"));
+        
+        System.out.println("Test #3 - middleThree('solving')");
+        System.out.printf("Output: %s\n", middleThree("solving"));
+    }
+    
     public static String middleThree(String str)
     {
         int len = str.length() / 2;
