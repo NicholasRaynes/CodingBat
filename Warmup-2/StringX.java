c5339f0ec811d77b8f6ce648142decfd196e1264
Nicholas Raynes <98918017+NicholasRaynes@users.noreply.github.com>
2022-05-23 14:20:11 -0500

    Completed documentation for the class.

--- Warmup-2/StringX.java
+++ Warmup-2/StringX.java
@@ -1,3 +1,11 @@
+/**
+* This class includes the declaration, implementation, and testing for
+* the stringX method.
+*
+* @author NicholasRaynes
+* @version 0.0.1
+* @see https://codingbat.com/prob/p171260
+*/
 public class StringX
 {
     public static void main(String[] args)
